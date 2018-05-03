a9685277d3098787ef7963f0226d8d38449d4640
chanoo104 <chanoo104@naver.com>
2018-05-04 00:13:12 +0900

    Add files via upload

--- response.js
+++ response.js
@@ -85,6 +85,10 @@ if(admin.indexOf(sender)>-1) {
 		if (msg == "!프사") {
    var img = ImageDB.getProfileImage();
    replier.reply(img);
+}
+if (msg == "!초기화"){
+	   replier.reply(sender+"님의 욕설 카운터를 초기화 시켰습니다.");
+	    DataBase.setDataBase("0", sender); //새로 저장
 }}
 else if(admin.indexOf(sender)==-1) {
 	if(msg.trim()=="!카운트"){
@@ -99,6 +103,9 @@ else if(admin.indexOf(sender)==-1) {
 	replier.reply("관리자가 아닙니다.")
 	}
 		if (msg == "!프사") {
+		replier.reply("관리자가 아닙니다.")
+	}
+			if (msg == "!초기화") {
 		replier.reply("관리자가 아닙니다.")
 	}
 }
@@ -179,10 +186,10 @@ if (msg.indexOf("!위키 ") == 0) {
             war = Number(DataBase.getDataBase(sender))+1
             if (data == undefined) { //이미 저장된게 없다면
                 DataBase.setDataBase("1", sender); //새로 저장
-                replier.reply("쓰읍! 방금 욕설이 감지 되었습니다! 욕설 사용에 주의 하십시오.\n"+sender+"님의 경고수는 1회 입니다\n 10회 이상시 강퇴될 수 있습니다.");
+                replier.reply("쓰읍! 방금 욕설이 감지 되었습니다! 욕설 사용에 주의 하십시오.\n"+sender+"님의 경고수는 1회 입니다.\n 10회 이상시 강퇴될 수 있습니다.");
             } else { //이미 저장된게 있다면,
                 DataBase.setDataBase(war, sender);
-                replier.reply("쓰읍! 방금 욕설이 감지 되었습니다! 욕설 사용에 주의 하십시오.\n"+sender+"님의 경고수는"+war+"입니다\n 10회 이상시 강퇴될 수 있습니다.");
+                replier.reply("쓰읍! 방금 욕설이 감지 되었습니다! 욕설 사용에 주의 하십시오.\n"+sender+"님의 경고수는 "+war+"회 입니다.\n 10회 이상시 강퇴될 수 있습니다.");
             }
         break;
         }
