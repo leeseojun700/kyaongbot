b8e1081a1ab3106661132ad0154e75a52286656e
chanoo104 <chanoo104@naver.com>
2018-05-11 19:56:23 +0900

    무시하는 거 수정

--- response.js
+++ response.js
@@ -206,9 +206,10 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
     var msg = msg.replace(/[^(가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z)]/gi, "");
     for (var n = 0; n < words.length; n++) {
         if (msg.indexOf(words[n]) != -1) {
-            if (msg.indexOf(notwords[n]) != -1) {
-                break;
-            }
+            for (var n = 0; n < notwords.length; n++) {
+                if (msg.indexOf(notwords[n]) != -1) {
+                    break;
+                }
             var data = DataBase.getDataBase(sender)
             war = Number(DataBase.getDataBase(sender)) + 1
             if (data == undefined) { //이미 저장된게 없다면
