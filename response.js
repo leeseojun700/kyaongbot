b93457d0975067dfcbe143fe4b514906fc710109
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-05-13 03:51:23 +0900

    최종 버그픽스 완료

--- response.js
+++ response.js
@@ -21,7 +21,7 @@ function getTimeStamp() {
   var s =
     leadingZeros(d.getFullYear(), 4) + '-' +
     leadingZeros(d.getMonth() + 1, 2) + '-' +
-    leadingZeros(d.getDate(), 2) + ' ' +
+    leadingZeros(d.getDate(), 2) + ', ' +
 
     leadingZeros(d.getHours(), 2) + ':' +
     leadingZeros(d.getMinutes(), 2) + ':' +
