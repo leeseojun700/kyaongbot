a11e7b4bbde7e54f1c53b2ed93a1e01f2bfbba36
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-05-13 04:27:35 +0900

    문구 수정

--- response.js
+++ response.js
@@ -131,7 +131,8 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
                          DataBase.setDataBase(DataBase.getDataBase("timenew"), "timeold")
                     }
                     DataBase.setDataBase(getTimeStamp(), "timenew");
-                    replier.reply(DataBase.getDataBase("timeold") + "\n~\n" + DataBase.getDataBase("timenew") + "\n\n" + Number(DataBase.getDataBase(room)) + "회");
+                    replier.reply(DataBase.getDataBase("timeold") + "\n~\n" + DataBase.getDataBase("timenew") + "\n\n총 채팅 수 " + Number(DataBase.getDataBase(room)) + "회 기록됨");
+                    replier.reply(채팅카운터 초기화!)
                     DataBase.setDataBase(0, room);
                 }
                 if (msg == "!리로드") {
