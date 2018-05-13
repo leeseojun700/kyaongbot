7d5167ccca6e18aee345bd38172af7bf80362154
chanoo104 <chanoo104@naver.com>
2018-05-13 22:18:38 +0900

    횟수 초과시 강퇴 요구 보내도록 바꿈

--- response.js
+++ response.js
@@ -313,6 +313,9 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
                     DataBase.setDataBase(war, sender);
                     replier.reply("[" + sender + "]\n욕설 사용 횟수: " + war + "회\n욕설 사용을 자제해 주세요.");
                 }
+                if (war > 20) {
+                    Api.replyRoom("간부방", "욕설 횟수가 초가하여 보냅니다.\n" + sender + "님이 욕설 횟수가 " + war + "회 되어 이 메시지가 보내졌습니다. 강퇴 바랍니다.")
+                }
                 break;
             }
         }
