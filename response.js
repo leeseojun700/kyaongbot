b24269f873d1a9c270b8ebe429eb99b05e1d20ff
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-05-14 23:00:31 +0900

    일단 고!

--- response.js
+++ response.js
@@ -234,6 +234,27 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
         if (msg == "!카운트") {
             replier.reply("어제 하루동안 " + DataBase.getDataBase("daycache") + "개,\n지난 한시간동안 " + DataBase.getDataBase("hourcache") + "개\n의 채팅이 올라옴")
         }
+     if (msg == "!식별코드 확인") {
+  if (inick.indexOf(sender) == -1) { //식별코드-닉네임 배열 안에 전송자의 닉네임이 있는지 확인
+    var tn1 = sender.indexOf(inick);
+    var tn2 = icode[tn1 - 1];
+    replier.reply(tn1 + tn2);
+  } else {
+    var tcheck = true; //while 탈출용 변수
+    while (tcheck === false) { //while 값이 false일 동안
+      var n = 0;
+      n = Math.floor(Math.random() * 9000 + 1000); //랜덤 네자리수 생성
+      if (n.indexOf(icode) == -1) { //생성한 숫자가 기존 식별코드 배열 안에 있는지 확인
+        inick.push(sender); //식별코드-닉네임 배열 안에 전송자의 닉네임 추가
+        icode.push(n); //식별코드 배열 안에 전송자의 닉네임 추가
+        replier.reply(sender + n);
+        var tcheck = true; //없으면 tcheck 값을 true로 바꾸고 while문 탈출
+      }
+    }
+  }
+
+
+}
         if (msg == "!공지") {
             replier.reply(DataBase.getDataBase("공지"))
             count[room] = 0
