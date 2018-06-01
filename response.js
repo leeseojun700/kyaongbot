e20be6d7a1bf68d05d718ce9855d55f53ca2010b
rgb-odosk <38985060+rgb-odosk@users.noreply.github.com>
2018-06-02 04:18:02 +0900

    Update response.js

--- response.js
+++ response.js
@@ -139,7 +139,7 @@ if (DB.inick.indexOf(sender) == -1) {
       DB.inick.push(sender);
       DB.icode.push(String(n));
       replier.reply(DB.inick)
-      const ta = DB.inick
+      let ta = DB.inick
       var ta2 = []
       DB.inick.sort();
       replier.reply(ta)
